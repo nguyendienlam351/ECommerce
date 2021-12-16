ab7c04f7c68233fdd57c46aa428e6c0656d52a03
nguyendienlam351 <nguyendienlam351@gmail.com>
2021-12-16 19:36:45 +0700

    categoty list

---
+++ app/src/main/java/com/example/ecommerce/adapters/NewProductAdapter.java
@@ -0,0 +1,34 @@
+package com.example.ecommerce.adapters;
+
+import android.content.ContentUris;
+import android.content.Context;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class NewProductAdapter extends RecyclerView.Adapter<NewProductAdapter.ViewHolder> {
+    Context context;
+    List<>
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+    }
+}
--- app/src/main/java/com/example/ecommerce/fragments/HomeFragment.java
+++ app/src/main/java/com/example/ecommerce/fragments/HomeFragment.java
@@ -2,8 +2,12 @@ package com.example.ecommerce.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,16 +16,29 @@ import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.ecommerce.R;
+import com.example.ecommerce.adapters.CategoryAdapter;
+import com.example.ecommerce.models.CategoryModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    //Category recyclerview
+    RecyclerView catRecyclerView;
+    CategoryAdapter categoryAdapter;
+    List<CategoryModel> categoryModels;
+
+    FirebaseFirestore db;
+
     public HomeFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,6 +47,9 @@ public class HomeFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
         ImageSlider imageSlider = root.findViewById(R.id.image_slider);
+        catRecyclerView = root.findViewById(R.id.rec_category);
+
+        db = FirebaseFirestore.getInstance();
 
         List<SlideModel> slideModels = new ArrayList<>();
 
@@ -38,6 +58,29 @@ public class HomeFragment extends Fragment {
         slideModels.add(new SlideModel(R.drawable.banner3, "70% OFF", ScaleTypes.CENTER_CROP));
 
         imageSlider.setImageList(slideModels);
+
+        //Category
+        catRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
+        categoryModels = new ArrayList<>();
+        categoryAdapter = new CategoryAdapter(getActivity(), categoryModels);
+        catRecyclerView.setAdapter(categoryAdapter);
+
+        db.collection("Category")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                CategoryModel categoryModel = document.toObject(CategoryModel.class);
+                                categoryModels.add(categoryModel);
+                                categoryAdapter.notifyDataSetChanged();
+                            }
+                        } else {
+                        }
+                    }
+                });
+
         return root;
     }
 }
