86c9c3005863ee5ccba3ffff97059bdf7d28387b
nguyendienlam351 <nguyendienlam351@gmail.com>
2021-12-16 22:08:22 +0700

    new product list

--- app/src/main/java/com/example/ecommerce/adapters/NewProductAdapter.java
+++ app/src/main/java/com/example/ecommerce/adapters/NewProductAdapter.java
@@ -1,34 +1,58 @@
 package com.example.ecommerce.adapters;
 
+
 import android.content.ContentUris;
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.ecommerce.R;
+import com.example.ecommerce.models.NewProductModel;
 
 import java.util.List;
 
 public class NewProductAdapter extends RecyclerView.Adapter<NewProductAdapter.ViewHolder> {
     Context context;
-    List<>
+    List<NewProductModel> list;
+
+    public NewProductAdapter(Context context, List<NewProductModel> list) {
+        this.context = context;
+        this.list = list;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.new_products,parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        Glide.with(context).load(list.get(position).getImg_url()).into(holder.newImg);
+        holder.newName.setText(list.get(position).getName());
+        holder.newPrice.setText(String.valueOf(list.get(position).getPrice()));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView newImg;
+        TextView newName, newPrice;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            newImg = itemView.findViewById(R.id.new_img);
+            newName = itemView.findViewById(R.id.new_product_name);
+            newPrice = itemView.findViewById(R.id.new_price);
+        }
     }
 }
--- app/src/main/java/com/example/ecommerce/fragments/HomeFragment.java
+++ app/src/main/java/com/example/ecommerce/fragments/HomeFragment.java
@@ -11,13 +11,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.ecommerce.R;
 import com.example.ecommerce.adapters.CategoryAdapter;
+import com.example.ecommerce.adapters.NewProductAdapter;
 import com.example.ecommerce.models.CategoryModel;
+import com.example.ecommerce.models.NewProductModel;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -29,10 +32,15 @@ import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    RecyclerView catRecyclerView, newProductRecyclerView;
+
     //Category recyclerview
-    RecyclerView catRecyclerView;
     CategoryAdapter categoryAdapter;
     List<CategoryModel> categoryModels;
+
+    //New Products recyclerview
+    NewProductAdapter newProductAdapter;
+    List<NewProductModel> newProductModels;
 
     FirebaseFirestore db;
 
@@ -48,6 +56,7 @@ public class HomeFragment extends Fragment {
 
         ImageSlider imageSlider = root.findViewById(R.id.image_slider);
         catRecyclerView = root.findViewById(R.id.rec_category);
+        newProductRecyclerView = root.findViewById(R.id.new_product_rec);
 
         db = FirebaseFirestore.getInstance();
 
@@ -77,9 +86,34 @@ public class HomeFragment extends Fragment {
                                 categoryAdapter.notifyDataSetChanged();
                             }
                         } else {
+                            Toast.makeText(getActivity(), "" + task.getException(), Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
+
+        //New Products
+        newProductRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
+        newProductModels = new ArrayList<>();
+        newProductAdapter = new NewProductAdapter(getActivity(), newProductModels);
+        newProductRecyclerView.setAdapter(newProductAdapter);
+
+        db.collection("NewProducts")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                NewProductModel newProductModel = document.toObject(NewProductModel.class);
+                                newProductModels.add(newProductModel);
+                                newProductAdapter.notifyDataSetChanged();
+                            }
+                        } else {
+                            Toast.makeText(getActivity(), "" + task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
 
         return root;
     }
